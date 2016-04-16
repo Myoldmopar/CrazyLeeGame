6e5eb5d28af538df256e0de3e29ba5f9ec39dcd0
Edwin Lee <leeed2001@gmail.com>
2016-04-16 15:14:36 -0600

    Reorganize code, add fake _enemy_ hello pictures

---
+++ src/enemy.lua
@@ -0,0 +1,18 @@
+local enemy = {}
+
+enemyImg = love.graphics.newImage("images/enemy.png")
+
+function enemy:new()
+	return {x = math.random(1, 100), y = math.random(1, 100), angle = math.random(5, 85), img = enemyImg}
+end
+
+function enemy:update(dt)
+	self.y = self.y + 10
+end
+
+function enemy:draw()
+	love.graphics.draw(self.x, self.y, self.img)
+end
+
+return enemy
+	
--- src/main.lua
+++ src/main.lua
@@ -4,6 +4,9 @@ gibson = { x = 400, y = 80, img = nil, win = false, orig_y = 80, can_shoot = tru
 bulletImg = nil
 bullets = {}
 min_y = 80
+enemies = {}
+finish_y = 800
+local enemyclass = require 'enemy'
 
 function love.load()
 	daxton.img = love.graphics.newImage('images/daxton.png')
@@ -16,7 +19,7 @@ function love.draw()
 	love.graphics.print("Move Gibson with m and k", 0, 20, 0, 1.5)
 	love.graphics.print("Exit with ESC", 0, 40, 0, 1.5)
 	love.graphics.print(" **** START: **************************************************************************************", 0, 60)
-	love.graphics.print(" **** FINISH: **************************************************************************************", 0, 500)
+	love.graphics.print(" **** FINISH: **************************************************************************************", 0, finish_y)
 	love.graphics.draw(gibson.img, gibson.x, gibson.y)
 	if gibson.win then
 		love.graphics.print("GIBSON WINS - PRESS R", 250, 0, 0, 3)
@@ -28,9 +31,24 @@ function love.draw()
 	for i, bullet in ipairs(bullets) do
 		love.graphics.draw(bullet.img, bullet.x, bullet.y)
 	end
+	for i, enemy in ipairs(enemies) do
+		love.graphics.draw(enemy.img, enemy.x, enemy.y)
+	end
 end
 
 function love.update(dt)
+	if math.random() > 0.95 then
+		thisenemy = enemyclass:new()
+		table.insert(enemies, thisenemy)
+	end
+	for i, enemy in ipairs(enemies) do
+		--print(enemy)
+		--love.event.push('quit')
+		enemy.y = enemy.y + 10
+		if enemy.y > finish_y then
+			table.remove(enemies, i)
+		end
+	end
 	gibson.canShootTimer = gibson.canShootTimer - dt
 	if gibson.canShootTimer < 0 then
  		gibson.canShoot = true
@@ -41,7 +59,7 @@ function love.update(dt)
 	end
 	for i, bullet in ipairs(bullets) do
 		bullet.y = bullet.y + (250 * dt)
-		if bullet.y > 500 then -- remove bullets when they pass off the screen
+		if bullet.y > finish_y then -- remove bullets when they pass off the screen
 			table.remove(bullets, i)
 		end
 	end	
@@ -81,7 +99,7 @@ function love.update(dt)
 	elseif love.keyboard.isDown('k') then
 		gibson.y = gibson.y - increment
 	end
-	winning_y = 280
+	winning_y = finish_y - 220
 	gibson.y = math.max( gibson.y, 80 )
 	daxton.y = math.max( daxton.y, 80 )
 	if gibson.y > winning_y then
